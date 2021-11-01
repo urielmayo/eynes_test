f366465c76bef01ef5d1902042d163d782f54a75
urielmayo <urimayo00@gmail.com>
2021-10-31 23:30:13 -0300

    adding testing environment

--- simple/main.py
+++ simple/main.py
@@ -1,15 +1,13 @@
-from random import randint
 
-def main():
-    """ Function running de excersise"""
-    ids_list = []
-    for i in range(10):
-        age = randint(1, 100)
-        ids_list.append({'id': i, 'age':age})
-    
-    return ids_list
+import unittest
+
+example_list = set
+
+class Test(unittest.TestCase):
+    def_test
+
+
+
 
 if __name__=='__main__':
-    ids_list = main()
-    print("printing ids_list:")
-    print(ids_list)
+    unittest.main(verbosity=3)
