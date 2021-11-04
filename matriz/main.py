ee91b56df9c91e4f3c88f9bc71adab5b8f5cefa4
urielmayo <urimayo00@gmail.com>
2021-11-04 00:06:43 -0300

    changing list of aligned numbers to set; adding one more test

--- matriz/main.py
+++ matriz/main.py
@@ -9,8 +9,10 @@ def gen_matrix():
 
             
 def get_four_in_line(matrix):
+    """ This methods loops around the matrix and finds if there are 4 numbers align verically/horizontally"""
     transposed_matrix = [[0 for i in range(5)] for j in range(5)]
-    four_in_line_list = []
+    four_in_line_set = set() # we use a set so we dont have to put the 4-in-line items in order when testing 
+    
     for row in range(5):
         for col in range(5):
             transposed_matrix[row][col]=matrix[col][row]
@@ -21,19 +23,19 @@ def get_four_in_line(matrix):
             #since matrix is 5x5 and we need to get 4-in-line. we only need to analize the first 2 items back and forth
             item = matrix[row][col] 
             if matrix[row][col:offset + col] == [item, item + 1, item + 2, item + 3]: #checking 4 in line from 1st to 4th and 2nd to 5th element
-                four_in_line_list.append(((row,col),(row,col + offset -1)))
+                four_in_line_set.add(((row,col),(row,col + offset -1)))
             elif matrix[row][col:offset + col] == [item , item - 1, item -2, item-3]: #checking 4 in line from 1st to 4th and 2nd to 5th element backwards
-                four_in_line_list.append(((row,col + offset -1),(row,col)))
+                four_in_line_set.add(((row,col + offset -1),(row,col)))
     
     #we repeat the process with the transposed matrix
     for row in range(5):
         for col in range(2):
             item = transposed_matrix[row][col]
             if transposed_matrix[row][col:offset + col] == [item, item + 1, item + 2, item + 3]:
-                four_in_line_list.append(((row,col),(row,col + offset -1)))
+                four_in_line_set.add(((row,col),(row,col + offset -1)))
             elif transposed_matrix[row][col:offset + col] == [item , item - 1, item -2, item-3]:
-                four_in_line_list.append(((row,col + offset -1),(row,col)))
-    return four_in_line_list
+                four_in_line_set.add(((row,col + offset -1),(row,col)))
+    return four_in_line_set
 
 def print_matrix(matrix):
     for row in matrix:
--- matriz/testing.py
+++ matriz/testing.py
@@ -10,21 +10,33 @@ class Test(unittest.TestCase):
             [5, 2, 4, 5, 2]
         ]
         self.assertEqual(
-            [((0, 0), (0, 3)), ((4, 4), (4, 1))], 
+            {((4, 4), (4, 1)), ((0, 0), (0, 3))}, 
             get_four_in_line(matrix)
         )
     def test_2(self):
             matrix = [
-                [2, 3, 4, 5, 1],
-                [5, 3, 3, 5, 5],
-                [2, 2, 1, 4, 4],
-                [5, 1, 3, 4, 3],
-                [5, 2, 4, 5, 2]
+                [2, 3, 3, 5, 1],
+                [5, 2, 3, 5, 5],
+                [2, 3, 4, 5, 4],
+                [5, 4, 3, 4, 3],
+                [5, 5, 4, 5, 2]
             ]
             self.assertEqual(
-                [((0, 0), (0, 3)), ((4, 4), (4, 1))], 
+                {((2, 0), (2, 3)), ((1, 1), (1, 4)), ((4,4),(4,1))},
+                get_four_in_line(matrix)
+            )
+    def test_3(self):
+            matrix = [
+                [2, 3, 3, 5, 1],
+                [5, 2, 3, 5, 5],
+                [1, 3, 4, 5, 1],
+                [5, 1, 3, 4, 3],
+                [5, 5, 4, 5, 2]
+            ]
+            self.assertEqual(
+                set(),
                 get_four_in_line(matrix)
             )
 
 if __name__=='__main__':
-    unittest.main(verbosity=3)
+    unittest.main(verbosity=2)
